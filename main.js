ec5fbecd07040d7cf814335057afec1d4ebf82fb
Evan Oyster <oyster.evan@gmail.com>
2022-09-01 17:16:19 -0400

    another question

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const input = document.querySelector('input');
 const select = document.querySelector('button');
-
+const checkBox = document.createElement('input');
+checkBox.type='checkbox';
+checkBox.style.cssFloat='left';
+const deleteButton = document.createElement('button')
+deleteButton.classList = 'button'
 
 select.addEventListener('click', addItem);
 
 function addItem() {
-    const list = document.getElementById('list');
-    const inputElement = document.createElement('li')
+    const list = document.getElementById('script');
+    const inputElement = document.createElement('li');
     const newLi = document.createTextNode(input.value);
-    inputElement.appendChild(newLi);
-    document.body.insertBefore(inputElement, list)
+    inputElement.append(newLi, checkBox, deleteButton);
+    document.body.insertBefore(inputElement, list);
+    checkBox.addEventListener('change', () => {
+        if (checkBox.checked){
+        inputElement.classList = 'line';
+        } else {
+            inputElement.classList = 'noline'
+        }
+    })
+    
+    
+
 }
-
-
