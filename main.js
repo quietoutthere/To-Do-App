17df7da64ee0e1fff2d4094ffa56cbaf5b0f577f
Evan Oyster <oyster.evan@gmail.com>
2022-09-26 14:55:17 -0400

    finally did some shit

--- main.js
+++ main.js
@@ -1,27 +1,59 @@
-const input = document.querySelector('input');
-const select = document.querySelector('button');
-const checkBox = document.createElement('input');
-checkBox.type='checkbox';
-checkBox.style.cssFloat='left';
-const deleteButton = document.createElement('button')
-deleteButton.classList = 'button'
-
-select.addEventListener('click', addItem);
+const inputButton = document.getElementById('inputButton');
+inputButton.addEventListener('click', addItem);
 
 function addItem() {
-    const list = document.getElementById('script');
-    const inputElement = document.createElement('li');
-    const newLi = document.createTextNode(input.value);
-    inputElement.append(newLi, checkBox, deleteButton);
-    document.body.insertBefore(inputElement, list);
-    checkBox.addEventListener('change', () => {
-        if (checkBox.checked){
-        inputElement.classList = 'line';
-        } else {
-            inputElement.classList = 'noline'
-        }
-    })
+    const inputItem = document.getElementById('inputItem');
+    const itemText = inputItem.value;
     
-    
+    const newItem = createListItem(itemText);
 
+    const list = document.getElementById('list');
+    list.appendChild(newItem)
 }
+
+function createListItem(itemText) {
+    const lineItem = document.createElement('li');
+    lineItem.setAttribute('id', Math.random());
+    lineItem.style = 'list-style-type: none';
+
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.addEventListener('change', handleCheckBox);
+    lineItem.appendChild(checkBox);
+    
+    const lineItemText = document.createElement('span');
+    lineItemText.textContent = itemText;
+    lineItemText.className = 'itemText';
+    lineItem.appendChild(lineItemText);
+
+    //const lineItemButton = document.createElement('img');
+
+    return lineItem;
+}
+
+function handleCheckBox(event) {
+    event.preventDefault();
+    const target = event.target;
+    target.nextSibling.style = target.checked ? 
+    'text-decoration: line-through' : 
+    'text-decoration: none';
+}
+
+//const list = document.getElementById('script');
+//const inputElement = document.createElement('li');
+//const newLi = document.createTextNode(input.value);
+//inputElement.append(newLi, checkBox, deleteButton);
+//document.body.insertBefore(inputElement, list);
+//checkBox.addEventListener('change', () => {
+    //if (checkBox.checked){
+    //inputElement.classList = 'line';
+    //} else {
+       // inputElement.classList = 'noline'
+   // }
+//})
+
+//const checkBox = document.createElement('input');
+//checkBox.type='checkbox';
+//checkBox.style.cssFloat='left';
+//const deleteButton = document.createElement('button')
+//deleteButton.classList = 'button'
