2ecf4cadd0a739d351caaf997e1d5dd6c2f7296a
Evan Oyster <oyster.evan@gmail.com>
2022-09-29 16:16:24 -0400

    added delte button

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ function addItem() {
     const inputItem = document.getElementById('inputItem');
     const itemText = inputItem.value;
     inputItem.value = ''
-    
     const newItem = createListItem(itemText);
-
     const list = document.getElementById('list');
     list.appendChild(newItem)
 }
@@ -29,8 +27,16 @@ function createListItem(itemText) {
 
     const lineItemButton = document.createElement('img');
     lineItemButton.setAttribute('src', 'images/delete.png');
+    lineItemButton.addEventListener('click', removeElement);
+    lineItemButton.style.height = '25px'
     listItem.appendChild(lineItemButton)
     return listItem;
+}
+
+function removeElement(event) {
+    event.preventDefault();
+    const trash = event.target;
+    trash.parentElement.remove();
 }
 
 function handleCheckBox(event) {
@@ -40,22 +46,3 @@ function handleCheckBox(event) {
     'text-decoration: line-through' : 
     'text-decoration: none';
 }
-
-//const list = document.getElementById('script');
-//const inputElement = document.createElement('li');
-//const newLi = document.createTextNode(input.value);
-//inputElement.append(newLi, checkBox, deleteButton);
-//document.body.insertBefore(inputElement, list);
-//checkBox.addEventListener('change', () => {
-    //if (checkBox.checked){
-    //inputElement.classList = 'line';
-    //} else {
-       // inputElement.classList = 'noline'
-   // }
-//})
-
-//const checkBox = document.createElement('input');
-//checkBox.type='checkbox';
-//checkBox.style.cssFloat='left';
-//const deleteButton = document.createElement('button')
-//deleteButton.classList = 'button'
