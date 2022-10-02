971a12278debd2c38d0dde5872f1ec8f169865d7
Evan Oyster <oyster.evan@gmail.com>
2022-10-02 10:30:33 -0400

    fixed line items

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+//description: create two todo items and
+
+//step 1: create a todo item called 'item 1'
+//EXPECTED: todo item called item 1 shows up on screen
+//ACTUAL: IT WORKS
+
+//step 2: create a second todo item called 'item 2'
+//EXPECTED: todo item called item 2 shows up below item 1
+//ACTUAL: IT WORKS
+
+//step 3: click on second todo item's checkbox
+//EXPECTED: there should be a line through for item 2, a garbage icon should appear
+//ACTUAL: IT WORKS
+
+//step 4: click on second todo item's garbage icon
+//EXPECTED: second todo item should dissapear
+//ACTUAL: IT DOESN'T WORK; both todo items get deleted
+
 const inputButton = document.getElementById('inputButton');
-const listItem = document.createElement('li');
 const inputItem = document.getElementById('inputItem');
 const form = document.getElementById('form');
-//inputButton.addEventListener('click', addItem);
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -18,6 +34,7 @@ function addItem() {
 }
 
 function createListItem(itemText) {
+    const listItem = document.createElement('li')
     listItem.setAttribute('id', Math.random());
     listItem.style = 'list-style-type: none';
 
@@ -34,6 +51,7 @@ function createListItem(itemText) {
 }
 
 function addButton() {
+    const listItem = document.createElement('li')
     const lineItemButton = document.createElement('img');
     lineItemButton.setAttribute('src', 'images/delete.png');
     lineItemButton.addEventListener('click', removeElement);
@@ -55,3 +73,4 @@ function handleCheckBox(event) {
     'text-decoration: none';
     addButton();
 }
+
