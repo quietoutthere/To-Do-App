326aa8cc82d292c14570b0453cb37ee2d1493766
Evan Oyster <oyster.evan@gmail.com>
2022-10-17 11:17:54 -0400

    review updates

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-A helpful to do application
+# A helpful to do application
 
 Use it here [here](https://quietoutthere.github.io/To-Do-App/)
 
 ![to do app](https://user-images.githubusercontent.com/108839805/193134456-f200d17a-9ac0-40e2-b82f-10acd09a638a.png)
 
+## Description
+
 This was my 3rd project using Java Script. It was an introduction to creating elements, understanding ULs as well as understanding the concept of siblings, and parent nodes.
 
-Author: Evan Oyster - Full Stack Developer
-
+## Author
+Evan Oyster - Full Stack Developer
 [LinkedIn](https://www.linkedin.com/feed/)
--- main.js
+++ main.js
@@ -4,21 +4,24 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    if (inputItem.value.trim().length === 0) {
+        inputItem.value = '';
+        return
+    }
     addItem();
 })
 
 function addItem() {
     const itemText = inputItem.value;
-    inputItem.value = ''
+    inputItem.value = '';
     const newItem = createListItem(itemText);
     const list = document.getElementById('list');
     list.appendChild(newItem);
 }
 
 function createListItem(itemText) {
-    const listItem = document.createElement('li')
-    listItem.setAttribute('id', Math.random());
-    listItem.style = 'list-style-type: none';
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'list-item');
 
     const checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
@@ -27,45 +30,41 @@ function createListItem(itemText) {
     });
     listItem.appendChild(checkBox);
     
-    const listItemText = document.createElement('span');
+    const listItemText = document.createElement('div');
     listItemText.textContent = itemText;
-    listItemText.className = 'itemText';
+    listItemText.className = 'item-text';
     listItem.appendChild(listItemText);
     return listItem;
 }
 
-function addButton(li) {
+function addButton(event) {
     const listItem = document.createElement('li')
     const lineItemButton = document.createElement('img');
     lineItemButton.id = 'trash-icon';
     lineItemButton.setAttribute('src', 'images/delete.png');
     lineItemButton.addEventListener('click', removeElement);
-    lineItemButton.style.height = '25px';
-    li.appendChild(lineItemButton);
+    event.target.parentElement.appendChild(lineItemButton);
 }
 
 function removeElement(event) {
-    event.preventDefault();
     const trash = event.target;
     trash.parentElement.remove()
    }
 
-function handleCheckBox(event, li) {
+function handleCheckBox(event) {
     event.preventDefault();
     const target = event.target;
     const activate = target.checked
     if (activate === true) {
         target.nextSibling.style.textDecoration = 'line-through';
-        addButton(li)
+        addButton(event)
     } else {
         target.nextSibling.style.textDecoration = 'none';
-        removeButton(event, target)
+        removeButton(event)
     }
 }
 
-function removeButton(event, target) {
-    event.preventDefault();
-    const trash = event.target;
+function removeButton(event) {
     const trashIcontElement = target.parentElement.querySelector('#trash-icon');
-    target.parentElement.removeChild(trashIcontElement);
+    event.target.parentElement.removeChild(trashIcontElement);
 }
